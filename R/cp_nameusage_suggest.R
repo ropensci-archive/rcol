68798bca65468e8014f8584f4647a1609885a234
Scott Chamberlain <myrmecocystus@gmail.com>
2020-02-27 08:22:37 -0800

    fix name suggest fxn, dataestkey is required

--- R/cp_nameusage_suggest.R
+++ R/cp_nameusage_suggest.R
@@ -1,23 +1,23 @@
 #' Name Usage: Suggest
 #'
 #' @export
-#' @param q (character) main query string
-#' @param datasetKey (character) dataset key
+#' @param q (character) main query string. required
+#' @param datasetKey (character) dataset key. required
 #' @param vernaculars (logical) Whether or not to include vernacular names
 #' in the suggestions (default: `FALSE`)
 #' @param limit (integer) requested number of maximum records to be returned.
 #' Default: 10; max: 1000
 #' @param ... curl options passed on to [crul::verb-GET]
-cp_nameusage_suggest <- function(q = NULL, datasetKey = NULL,
-  vernaculars = FALSE, limit = 10, ...) {
-
-  stop("AFAICT this route isn't working yet")
+#' @return a tibble
+#' @examples \dontrun{
+#' cp_nameusage_suggest(q="Apis", datasetKey = 3)
+#' }
+cp_nameusage_suggest <- function(q, datasetKey, vernaculars = FALSE, limit = 10,
+  ...) {
 
   assert(limit, c("numeric", "integer"))
   args <- cc(list(q = q, datasetKey = datasetKey, vernaculars = vernaculars,
     limit = limit))
   tmp <- cp_GET(colplus_base(), "nameusage/suggest", query = args, ...)
-  tmp$result <- tibble::as_tibble(tmp$result)
-  tmp <- cp_meta(tmp)
-  return(tmp)
+  return(tibble::as_tibble(tmp$suggestions))
 }
